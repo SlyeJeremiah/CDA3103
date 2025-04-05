468316173b4f5a343591482bd921c6f53eefd6d0
SlyeJeremiah <155405463+SlyeJeremiah@users.noreply.github.com>
2025-04-05 18:18:52 -0400

    Update tinyvm
    Added print statements. Feels like something is missing but i havent gone over compiling and running, and i havent tested anything yet.

--- tinyvm.c
+++ tinyvm.c
@@ -34,6 +34,11 @@ int main(int argc, char **file) {
         index++;
     }
 
+    printf("Initial values\n");
+    fprintf(OutFile, "Initial values\n");
+    printf("PC = %d | A = %d | DM = [%d,%d,%d,%d,%d,%d,%d,%d,%d,%d]\n\n", PC, A, DM[0],DM[1],DM[2],DM[3],DM[4],DM[5],DM[6],DM[7],DM[8],DM[9]);
+    fprintf(OutFile, "PC = %d | A = %d | DM = [%d,%d,%d,%d,%d,%d,%d,%d,%d,%d]\n\n", PC, A, DM[0],DM[1],DM[2],DM[3],DM[4],DM[5],DM[6],DM[7],DM[8],DM[9]);
+
     while ( EOP == 1 ) {
 
         // FETCH
@@ -46,6 +51,9 @@ int main(int argc, char **file) {
         {
             case 1:
                 // LOAD <addr>
+                printf("LOAD <%d>\n", IR.addr);
+                fprintf(OutFile, "LOAD <%d>\n", IR.addr);
+
                 MAR = IR.addr;
                 MDR = DM[MAR];
                 A = MDR;
@@ -53,6 +61,9 @@ int main(int argc, char **file) {
     
             case 2: 
                 // ADD
+                printf("ADD <%d>\n", IR.addr); 
+                fprintf(OutFile, "ADD <%d>\n", IR.addr);
+
                 MAR = IR.addr;
                 MDR = DM[MAR];
                 A = A + MDR;
@@ -60,6 +71,9 @@ int main(int argc, char **file) {
 
             case 3:
                 // STORE
+                printf("STORE <%d>\n", IR.addr); 
+                fprintf(OutFile, "STORE <%d>\n", IR.addr);
+
                 MAR = IR.addr;
                 MDR = A;
                 DM[MAR] = MDR;
@@ -67,6 +81,9 @@ int main(int argc, char **file) {
 
             case 4:
                 // SUB
+                printf("SUB <%d>\n", IR.addr); 
+                fprintf(OutFile, "SUB <%d>\n", IR.addr);
+
                 MAR = IR.addr;
                 MDR = DM[MAR];
                 A = A - MDR;
@@ -74,26 +91,42 @@ int main(int argc, char **file) {
 
             case 5:
                 // IN <5>
+                printf("IN <5>\n");
+                fprintf(OutFile, "IN <5>\n");
+                printf("Input a value: ");
                 scanf("%d", &A);
+                fprintf(OutFile, "Input a value: %d", A);
+                printf("\n");
+                fprintf(OutFile, "\n");
                 break;
 
             case 6:
                 // OUT <7>
-                printf("%d", A);
+                printf("OUT <7>\nResult is: %d\n", A);
+                fprintf(OutFile, "OUT <7>\nResult is: %d\n");
                 break;
 
             case 7:
                 // HALT
+                printf("HALT\n"); 
+                fprintf(OutFile, "HALT\n");
+
                 EOP = 0;
                 break;
     
             case 8:
                 // JMP <addr>
+                printf("JMP <%d>\n", IR.addr); 
+                fprintf(OutFile, "JMP <%d>\n", IR.addr);
+
                 PC = IR.addr;
                 break;
 
             case 9:
                 // SKIPZ
+                printf("SKIPZ\n"); 
+                fprintf(OutFile, "SKIPZ\n");
+
                 if ( A == 0 ) {
                     PC = PC + 4;
                 }
@@ -101,6 +134,9 @@ int main(int argc, char **file) {
 
             case 10:
                 // SKIPG
+                printf("SKIPG\n"); 
+                fprintf(OutFile, "SKIPG\n");
+
                 if ( A > 0 ) {
                     PC = PC + 4;
                 }
@@ -108,14 +144,21 @@ int main(int argc, char **file) {
 
             case 11:
                 // SKIPL
+                printf("SKIPL\n"); 
+                fprintf(OutFile, "SKIPL\n");
+                
                 if ( A < 0 ) {
                     PC = PC + 4;
                 }
                 break;
         }
         // Print current state of PC, AC, DM after each instruction
-        fprintf(OutFile, "PC = %d | A = %d | DM = [%d,%d,%d,%d,%d,%d,%d,%d,%d,%d]", PC, A, DM[0],DM[1],DM[2],DM[3],DM[4],DM[5],DM[6],DM[7],DM[8],DM[9]);
+        fprintf(OutFile, "\nPC = %d | A = %d | DM = [%d,%d,%d,%d,%d,%d,%d,%d,%d,%d]\n\n", PC, A, DM[0],DM[1],DM[2],DM[3],DM[4],DM[5],DM[6],DM[7],DM[8],DM[9]);
     }
+
+    printf("End of Program.");
+    fprintf(OutFile, "End of Program.");
+
     //Close all files
     fclose(InputFile);
     fclose(OutFile);
